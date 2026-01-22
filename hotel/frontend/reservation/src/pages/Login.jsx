dc58eba7b1ffe54760cf93848c74c237dfa166aa
kit2824bad125 <kit28.24bad125@gmail.com>
2026-01-22 18:59:48 +0530

    Auth Css File

--- hotel/frontend/reservation/src/pages/Login.jsx
+++ hotel/frontend/reservation/src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { API } from "../api";
 import { useNavigate } from "react-router-dom";
-import "../styles/Auth.css";
+import "../Auth.css";
 
 export default function Login() {
   const navigate = useNavigate();
